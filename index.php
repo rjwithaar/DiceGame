a48ac753160f92c9fdf6de6957b95e415f9b5543
Robin Withaar <rj.withaar@alfa-college.nl>
2021-02-17 09:41:49 +0100

    include content pages

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 include_once('header.php');
 
+$page = 'content';
+if (isset($_GET['page']) && !empty($_GET['page'])) {
+    $page = $_GET['page'];
+}
+include(sprintf('pages/%s.php', $page));
+
 include_once('footer.php');
