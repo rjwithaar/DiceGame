ea9c12e274fc6c64d43bcf0dd2846f1800c7eaba
Robin Withaar <rj.withaar@alfa-college.nl>
2021-02-19 10:05:58 +0100

    Create first class and autoload all classes files

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
-include_once('header.php');
 
+// Set smart Directory Separator
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+
+// Include all classes
+foreach (glob(implode(DS, [__DIR__, 'functions', '*.php'])) as $file) {
+    require_once($file);
+}
+
+// Show page
+include_once('header.php');
 $page = 'content';
 if (isset($_GET['page']) && !empty($_GET['page'])) {
     $page = $_GET['page'];
 }
-include(sprintf('pages/%s.php', $page));
-
+include(sprintf(implode(DS, ['pages', '%s.php']), $page));
 include_once('footer.php');
