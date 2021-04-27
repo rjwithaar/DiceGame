c5f43847cd27295302e67457389581bb4d72d579
Robin Withaar <rj.withaar@alfa-college.nl>
2021-04-27 21:29:51 +0200

    Fix mobile bonus usage

--- script.js
+++ script.js
@@ -4,14 +4,5 @@
         $('form').on('change', 'input', function () {
             $('form').submit();
         });
-        $('.bonus-block').click(function () {
-            let input = $(this).next('div').find('input');
-            let value = input.val();
-            if (value > 0) {
-                value--;
-            }
-            input.val(value);
-            $('form').submit();
-        });
     });
 })(jQuery);
