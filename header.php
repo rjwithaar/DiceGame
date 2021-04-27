38579d31b2a0f33afc37983e9d143f5aff71df18
Robin Withaar <rj.withaar@alfa-college.nl>
2021-04-27 17:06:58 +0200

    Mobile optimisation

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="nl">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=35rem, initial-scale=0.68, minimum-scale=0.86, user-scalable=no">
     <title>SmartDiceGame</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet">
